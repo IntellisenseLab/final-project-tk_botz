fb8e0c84756e432713ead1129b09e0b78cb39aec
Tharaka Jayasena <9dmpires2k17.tuj@gmail.com>
2026-05-01 19:32:49 +0530

    add yaml config files to setup.py

--- ROS/botz_workspace/src/kobuki_driver/setup.py
+++ ROS/botz_workspace/src/kobuki_driver/setup.py
@@ -16,6 +16,8 @@ setup(
         # Install launch files
         (os.path.join('share', package_name, 'launch'), 
          glob(os.path.join('launch', '*.py'))),
+        (os.path.join('share', package_name, 'config'),
+         glob(os.path.join('config', '*.yaml'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
