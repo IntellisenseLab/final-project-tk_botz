9bbe64c8ad0aac13ed50c723592593a9c0a01b64
Tharaka Jayasena <9dmpires2k17.tuj@gmail.com>
2026-06-19 04:03:53 +0530

    worked with laptop

--- ROS/botz_workspace/src/kobuki_driver/launch/server_stack_launch.py
+++ ROS/botz_workspace/src/kobuki_driver/launch/server_stack_launch.py
@@ -2,7 +2,8 @@ import os
 
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource, XMLLaunchDescriptionSource
+from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
