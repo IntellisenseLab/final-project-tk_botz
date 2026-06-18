4f0a55045ba7a0679c7b429c39a0b7b8f891d517
Tharaka Jayasena <9dmpires2k17.tuj@gmail.com>
2026-06-18 23:17:36 +0530

    confgi change and seperate launch files

---
+++ ROS/botz_workspace/src/kobuki_driver/launch/server_stack_launch.py
@@ -0,0 +1,27 @@
+import os
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    kobuki_pkg_dir = get_package_share_directory('kobuki_driver')
+    slam_launch = os.path.join(kobuki_pkg_dir, 'launch', 'slam_launch.py')
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use simulation time',
+        ),    
+        
+        # SLAM Toolbox
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(slam_launch),
+            launch_arguments=[('use_sim_time', LaunchConfiguration('use_sim_time'))],
+        ),
+    ])
